e4d16dfa480209a1707bffa91fe8b67a1e9ed3bc
vitelo-code <roher86@gmail.com>
2024-03-12 23:54:31 -0600

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,43 @@
+// import logo_horizontal from '../images/logo/logo_horizontal.svg';
+import './navbar.css';
+
 function Navbar() {
   return (
     <div className='flex w-full h-16 justify-between bg-transparent'>
-      <div className='p-2 m-auto'>
+      <div className='m-auto items-center'>
         <h1 className='text-4xl font-serif font-bold'>
-          <a href='#Home'>Pandurata</a>
+          <a href='#Home'>
+            <h1 className='font-serif tracking-widest'>Pandurata</h1>
+            {/* TODO: Create new svg with only letters and update this. */}
+          </a>
         </h1>
       </div>
-      <div className='w-3/5 h-auto m-auto'>
+      <div className='w-3/5 m-auto'>
         <ul className='flex justify-evenly text-2xl font-sans font-bold'>
           <li>
-            <a href='#Home'>Inicio</a>
+            <a href='#Home' className='imgShadow'>
+              Inicio
+            </a>
           </li>
           <li>
-            <a href='#About'>Pandurata</a>
+            <a href='#About' className='imgShadow'>
+              Pandurata
+            </a>
           </li>
           <li>
-            <a href='#Menu'>Menú</a>
+            <a href='#Menu' className='imgShadow'>
+              Menú
+            </a>
           </li>
           <li>
-            <a href='#Location'>Ubicación</a>
+            <a href='#Location' className='imgShadow'>
+              Ubicación
+            </a>
           </li>
           <li>
-            <a href='#Reservations'>Reservaciones</a>
+            <a href='#Reservations' className='imgShadow'>
+              Reservaciones
+            </a>
           </li>
         </ul>
       </div>
